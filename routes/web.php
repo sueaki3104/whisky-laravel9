15644b3350359d6195b505bfef3a10a22a14f282
sueaki3104 <sueaki@a-omega.co.jp>
2023-03-15 17:28:31 +0900

    2023_0315_17:28_2ALL

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexCo
 Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
 ->name('tweet.update.put');
 
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
+->name('tweet.delete');
+
